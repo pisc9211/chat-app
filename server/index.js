f181c01bb02e1335cd8425e58e7a69bf4ed432f3
pisc9211 <pisc9211@gmail.com>
2019-10-11 16:01:06 -0400

    added start script in server as well as setting up a express server with socket-io

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const app = require('express')()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req,res) => res.send('Server is up and running!!!'))
+
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
