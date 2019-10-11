7b97be665e3d82308a8485c24c515a5c7ac6290d
pisc9211 <pisc9211@gmail.com>
2019-10-11 16:07:32 -0400

    added react app component and rendered to page

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { BrowswerRouter as Router, Route } from 'react-router-dom'
+import io from 'socket.io-client'
+
+import SignIn from './components/SignIn/SignIn'
+import Chat from './components/Chat/Chat'
+
+const App = () => {
+
+  return (
+    <Router>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/chat" component={Chat} />
+    </Router>
+  )
+}
+
+export default App
--- server/index.js
+++ server/index.js
@@ -6,4 +6,8 @@ const PORT = process.env.PORT || 5000
 
 app.get('/', (req,res) => res.send('Server is up and running!!!'))
 
+io.on('connection', (socket) => {
+
+})
+
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
